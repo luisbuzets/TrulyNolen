a54041e4bcd44bc48348fcb03f63386e432bab3a
luisbuzets <72575550+luisbuzets@users.noreply.github.com>
2023-11-20 08:56:12 -0600

    eliminado js

--- src/scrip.js
+++
@@ -1,8 +0,0 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
-const navbg = document.querySelector('.nav-bg');
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-    navbg.classList.toggle('active');
-});
