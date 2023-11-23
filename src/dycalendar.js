d702ac57e33346cda02ffe4a0cbef71d6447bb6b
luisbuzets <72575550+luisbuzets@users.noreply.github.com>
2023-11-23 17:24:28 -0600

    actualizado

--- src/dycalendar.js
+++ src/dycalendar.js
@@ -84,9 +84,11 @@ document.querySelector('#next-year').onclick = () => {
     generateCalendar(curr_month.value, curr_year.value)
 }
 
+/*
 let dark_mode_toggle = document.querySelector('.dark-mode-switch')
 
 dark_mode_toggle.onclick = () => {
     document.querySelector('body').classList.toggle('light')
     document.querySelector('body').classList.toggle('dark')
 }
+*/
